0969f8a96b1fd7f5b27c2549402ec66c70e6554e
Gagik Setikyan <setikyang@gmail.com>
2025-12-14 20:59:48 +0400

    Tasks 08.12.2025

---
+++ 08.12.2025/Timer.js
@@ -0,0 +1,51 @@
+/////////////////////////// Task 1
+
+// let count = 0;
+
+// let a = setInterval(() => {
+//   console.log(count++);
+//   if (count == 6) {
+//     clearInterval(a);
+//   }
+// }, 500);
+
+/////////////////////////////////Task 2
+
+// setTimeout(()=>{
+//     console.log("Hello");
+// },2000)
+
+//////////////////////////////// Task 3
+
+// let s = setInterval(() => {
+//   setTimeout(() => {
+//     clearInterval(s);
+//   }, 5000);
+//   console.log("Ping");
+// }, 1000);
+
+//////////////////////////////Task 4
+
+// function asyncDouble(n) {
+//   return new Promise((resolve) => {
+//     resolve(3);
+//   });
+// }
+
+// asyncDouble().then((val) => {
+//   setTimeout(() => {
+//     console.log(val * val);
+//   }, 300);
+// });
+
+//////////////////////////// Task 5
+
+// setTimeout(() => {
+//   console.log("First");
+// }, 100);
+// setTimeout(() => {
+//   console.log("Second");
+// }, 300);
+// setTimeout(() => {
+//   console.log("Third");
+// }, 500);
